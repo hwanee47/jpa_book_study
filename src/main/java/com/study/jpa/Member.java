1193ee96d9d4309a7c8561b819da6e75abc8396a
hwanee47 <>
2022-03-14 22:52:52 +0900

    4장 엔티티 매핑

--- src/main/java/com/study/jpa/Member.java
+++ src/main/java/com/study/jpa/Member.java
@@ -3,11 +3,14 @@ package com.study.jpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"}
+)})
 @Getter @Setter
 public class Member {
 
@@ -15,9 +18,27 @@ public class Member {
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10) // 필수로 입력, 10자를 초과하면 안됨.
     private String username;
 
     @Column(name = "AGE")
     private Integer age;
+
+
+    //==추가==
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+
+
+
 }
---
+++ src/main/java/com/study/jpa/RoleType.java
@@ -0,0 +1,5 @@
+package com.study.jpa;
+
+public enum RoleType {
+    ADMIN, USER
+}
